7adb244b582ceacdffbf55dc9b328813bc372b42
honey3go <honey3_go@163.com>
2017-03-15 23:05:19 +0800

    ep1level3

---
+++ JS/task0002_3.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dear_ on 2017/3/15.
+ */
+function $(id) {
+    return document.getElementById(id);
+}
+function rollPic(seconds){
+  var picMagLft = $("picUl").style.marginLeft;
+}
